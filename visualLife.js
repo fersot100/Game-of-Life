e1bfff546f67ca7fd23f29c5d23b38bad45773d5
Juan Erazo <fersot100@yahoo.com>
2017-09-23 17:12:15 -0700

    Adding play and pause funcionality

--- visualLife.js
+++ visualLife.js
@@ -1,34 +1,102 @@
-var gridArea = document.querySelector('#gridArea');
-
-function World(){
-
-}
-
+const gridArea = document.querySelector('#gridArea');
+let playing = false;
 function Grid(width, height){
 
-	this.grid = new Array(width * height).fill('');
+	this.map = new Array(width * height).fill('');
 	this.width = width;
 	this.height = height;
 
+	this.play = function(){
+		if (playing){
+			this.turn();
+			setTimeout(this.play.bind(this), 500);
+		}
+		
+	}
+
+	this.clear = function() {
+		this.map.fill();
+		console.log(this.htmlNodes.length)
+		this.htmlNodes.forEach(e => {
+			deactivate(this.map, e);
+		});
+	}
+
+	this.turn = function(){
+		console.log('Turn');
+		this.map.forEach((e,i) => {
+			let pos = this.itov(i);
+			let neighbors = 0;
+			for (let dir in directions){
+				let check = pos.plus(dir);
+				if(this.map[this.vtoi(check)] == '#')
+					neighbors++;
+			}
+			
+			if(e == '#'){
+				if(neighbors < 2 || neighbors > 3){
+					deactivate(this.map, this.htmlNodes[i]);
+					console.log('Deactivate', i);
+				}	
+			}else {
+				if(neighbors == 3){
+					activate(this.map, this.htmlNodes[i]);
+					console.log('Activate', i)
+				}	
+			}
+		});
+	}
+
+	this.itov = function (i) {
+		return new Vector(i % this.width, Math.floor(i / this.width));
+	}
+	this.vtoi = function (vector){
+		return vector.x + vector.y * this.width;
+	}
+	this.getValue = function (vector){
+		return this.map[vector.x + vector.y * this.width];
+	}
+
+	this.setValue = function (vector, value){
+		this.map[vector.x + vector.y * this.width] = value;
+	}
+
 	//Generate the html elements for the grid when a grid is made
-	for(let i = 1; i <= this.grid.length; i++){
+	for(let i = 1; i <= this.map.length; i++){
 		gridArea.innerHTML += `
 		<div data-index="${i-1}" class="square"></div>
 		${(i % this.width == 0) ? '<br>' : ''} 
 		`;
 	}
 
+	this.htmlNodes = gridArea.querySelectorAll('.square');
 	//Add a listener so when the user clicks a creature is created
 	gridArea.addEventListener('click', function(e){
 		if(!e.target.classList.contains('square'))
 			return;
 		if(e.target.classList.contains('active')){
-			deactivate(this.grid, e.target);
+			deactivate(this.map, e.target);
 		}else{
-			activate(this.grid, e.target);
+			activate(this.map, e.target);
 		}
 	}.bind(this));
 
+
+}
+
+function Vector(x, y) {
+	if(typeof x == 'string'){
+		return directions[x];
+	}else{
+		this.x = x;
+		this.y = y;
+		this.plus = function(v){
+			return new Vector(
+				this.x + v.x,
+				this.y + v.y
+				);
+		}
+	}
 }
 
 var directions = {
@@ -42,15 +110,6 @@ var directions = {
 	'nw': new Vector(-1,1)
 }
 
-function Vector(x, y) {
-	if(typeof x == 'string'){
-		this = directions[x];
-	}else{
-		this.x = x;
-		this.y = y;
-	}
-}
-
 function activate(grid, element){
 	grid[parseInt(element.dataset.index, 10)] = '';
 	element.classList.add('active');
@@ -62,3 +121,19 @@ function deactivate(grid, element) {
 }
 
 var g = new Grid(25,25);
+
+document.querySelector('#play').addEventListener('click',e => {
+	
+	if(!playing){
+		playing = true;
+		g.play();
+		e.target.innerText = 'Stop'
+	}else{
+		e.target.innerText = 'Play'
+		playing = false;
+	}
+
+});
+document.querySelector('#clear').addEventListener('click', e=> {
+	g.clear();
+})
